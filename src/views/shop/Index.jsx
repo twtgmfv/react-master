8363a4547c8889a8fb9cdfa6921dd1e0ebf5addf
xuruixue <xuruixue@jianlc.com>
2019-03-01 16:40:22 +0800

    first

---
+++ src/views/shop/Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Index extends React.Component {
+    render() {
+        return (
+            <div>
+                商城
+            </div>
+        );
+    }
+}
+
+export default Index;
