ea41e782f2220df0a16a6e7b6ed27e06a16fe46c
xuruixue <xuruixue@jianlc.com>
2019-03-28 23:07:53 +0800

    结合react

--- src/pages/Counter.js
+++ src/pages/Counter.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import {createStore, bindActionCreators, combineReducers} from "redux";
+import {Provider, connect} from "react-redux";
+
 
 let initailState = {
     count: 1
 };
 let person = {
-    name:'twt'
+    name: 'twt'
 }
 //reducer
 const reducerCounter = (state = initailState, action) => {
@@ -25,40 +27,69 @@ const reducerCounter = (state = initailState, action) => {
             break;
     }
 };
-const reducerPerson = (state=person,action) => state;
+const reducerPerson = (state = person, action) => state;
 
 
-// let store = createStore(reducerCounter);
-let store = createStore(combineReducers({reducerCounter,reducerPerson}));
+let store = createStore(reducerCounter);
+// let store = createStore(combineReducers({reducerCounter, reducerPerson}));
 
 let inc = function () {
     return {
         type: 'INCREMENT'
     }
 }
-inc = bindActionCreators(inc, store.dispatch)
+// inc = bindActionCreators(inc, store.dispatch)
 let dec = function () {
     return {
         type: 'DECREMENT'
     }
 }
-dec = bindActionCreators(dec, store.dispatch)
+// dec = bindActionCreators(dec, store.dispatch)
 store.subscribe(() => {
     console.log('state', store.getState());
 })
 // store.dispatch(inc())
-inc();
+// inc();
 // store.dispatch(dec())
-dec();
+// dec();
 
 import Style from './Counter.scss'
 
-export default class Counter extends React.Component {
+export class Counter extends React.Component {
     render() {
+        const {count, inc, dec} = this.props;
+        console.log(this.props);
         return (
             <div>
                 <h1>Counter Demo</h1>
+                数量 ：{count}
+                <button onClick={inc}>+</button>
+                <button onClick={dec}>-</button>
             </div>
         )
     }
+}
+
+
+function mapStateToProps(state) {
+    return {
+        count: state.count
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({inc,dec},dispatch)
+}
+
+const ConnectedCounter = connect(mapStateToProps,mapDispatchToProps)(Counter);
+
+export default class CounterSample extends React.Component{
+    render() {
+        return (
+            <Provider store={store}>
+                <ConnectedCounter/>
+            </Provider>
+        )
+    }
+
 }
